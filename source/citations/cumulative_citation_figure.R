493a0aaa9bea9412eaccbd86c19ee7afe5aad277
mvaudel <mvaudel@gmail.com>
2019-03-11 10:30:08 +0100

    Added code used to generate the citation figures

---
+++ source/citations/cumulative_citation_figure.R
@@ -0,0 +1,77 @@
+
+# Libraries
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(scico)
+
+
+# Load Data
+
+citationDF <- read.table("docs/citations/citation_table.txt", sep = "\t", stringsAsFactors = F, header = T)
+
+# Format
+
+citationDF <- citationDF %>% 
+    rename(Name = X) %>%
+    gather(key = "Year", value = "N", paste0("X", 1995:2018)) %>%
+    mutate("Year" = as.numeric(substring(Year, 2))) %>%
+    replace_na(list(N = 0))
+
+
+# Separate top 10 from others
+
+top10Lim <- sort(unique(citationDF$Sum), decreasing = T)[10]
+
+top10DF <- citationDF %>% filter(Sum >= top10Lim)
+bottomDF <- citationDF %>% filter(Sum < top10Lim)
+
+# Group others by year and merge
+
+otherDF <- bottomDF %>% 
+    group_by(Year) %>%
+    summarise(
+        Name = "Other",
+        PMID = paste(PMID, collapse = ", "),
+        Sum = sum(Sum),
+        Review = 11,
+        Update = 11,
+        Diff = 0,
+        N = sum(N)
+    )
+
+plotDF <- rbind(top10DF, otherDF)
+
+
+# Get ribbon values
+
+orderedSE <- c(unique(top10DF$Name[order(top10DF$Sum, decreasing = T)]), "Other")
+
+plotDF <- plotDF %>%
+    mutate(orderedName = factor(Name, levels = orderedSE)) %>%
+    group_by(Year) %>%
+    arrange(orderedName) %>%
+    mutate(
+        y2 = cumsum(N),
+        y1 = y2 - N
+    ) %>%
+    ungroup()
+
+# Plot
+
+cumulativePlot <- ggplot() + theme_bw(base_size = 18) +
+    geom_ribbon(data = plotDF, mapping = aes(x = Year, ymin = y1, ymax = y2, fill = orderedName)) +
+    scale_fill_manual(name = "Search Engine", values = c(scico(n = 10, palette = "batlow"), "grey80"), breaks = rev(orderedSE)) +
+    scale_y_continuous(name = "# Citations", expand = c(0, 0), limits = c(0, 1500)) +
+    scale_x_continuous(breaks = 1995:2018, expand = c(0, 0)) +
+    theme(
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        panel.grid.minor.x = element_blank()
+    )
+
+png("docs/figures/cumulative.png", width = 1600, height = 900)
+plot(cumulativePlot)
+dummy <- dev.off()
+
---
+++ source/citations/rank_figure.R
@@ -0,0 +1,62 @@
+
+# Libraries
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(scico)
+library(ggrepel)
+
+
+# Load Data
+
+citationDF <- read.table("docs/citations/citation_table.txt", sep = "\t", stringsAsFactors = F, header = T)
+
+# Format
+
+citationDF <- citationDF %>% 
+    rename(Name = X) %>%
+    mutate(
+        Sum2Y = X2017 + X2018,
+        rank = rank(-Sum),
+        rank2Y = rank(-Sum2Y, ties.method = "min"),
+        delta = rank - rank2Y
+    ) %>%
+    select(Name, Sum, rank, Sum2Y, rank2Y, delta) %>%
+    arrange(rank2Y) %>%
+    mutate(
+        rankName = paste(rank2Y, Name, sep = "- "),
+        rankNameOrdered = factor(rankName, levels = rev(rankName))
+    )
+
+# Plot
+
+scatterPlot <- ggplot() + theme_bw(base_size = 18) +
+    geom_abline(slope = 1, intercept = 0, col = "grey80", linetype = "dashed") +
+    geom_point(data = citationDF, mapping = aes(x = rank, y = rank2Y)) +
+    scale_x_reverse(name = "Rank [1995-2018]", expand = c(0, 0), limits = c(nrow(citationDF) + 5, 0)) +
+    scale_y_reverse(name = "Rank [2017-2018]", expand = c(0, 0), limits = c(nrow(citationDF) + 5, 0))
+
+png("docs/figures/rank.png", width = 1600, height = 900)
+plot(scatterPlot)
+dummy <- dev.off()
+
+maxDelta <- max(citationDF$delta)
+minDelta <- min(citationDF$delta)
+absDelta <- max(abs(minDelta), maxDelta)
+totalLength <- 2 * absDelta
+
+barPlot <- ggplot() + theme_bw(base_size = 18) +
+    geom_col(data = citationDF, mapping = aes(x = rankNameOrdered, y = delta, fill = delta), col = "black") +
+    scale_fill_scico(palette = "cork", begin = (-absDelta - minDelta) / totalLength, end = 1- (absDelta - maxDelta) / totalLength) +
+    coord_flip() +
+    scale_y_continuous(name = "Rank Gain") +
+    theme(axis.title.y = element_blank())
+
+png("docs/figures/rank_delta.png", width = 450, height = 800)
+plot(barPlot)
+dummy <- dev.off()
+
+
+
+
