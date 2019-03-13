723523ca67095ab7a85b91ed37e76716e64db5cd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-13 15:50:24 +0000

    Deploy ypriverol/CompProt2019-paper to github.com/ypriverol/CompProt2019-paper.git:gh-pages

---
+++ citations/cumulative_citation_figure.R
@@ -0,0 +1,68 @@
+##
+#
+# This scripts plots the cumulative number of citations for publications based on an export from Thomson Reuters™ Web of Science™.
+# Paths are relative to the repository folder.
+#
+##
+
+
+# Libraries
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(scico)
+
+
+# Load Data
+
+citationRaw <- read.table("../../data/web-sciences-metrics.txt", sep = ",", stringsAsFactors = F, header = T)
+
+# Format
+
+citationDF <- citationRaw[, paste0("X", 1995:2018)]
+citationDF$Name <- citationRaw[, 1]
+citationDF$total <- rowSums(citationDF[, paste0("X", 1995:2018)])
+
+citationDF <- citationDF %>%
+    arrange(desc(total))
+
+plotDF <- citationDF %>%
+    gather(key = "Year", value = "N", paste0("X", 1995:2018)) %>%
+    mutate("Year" = as.numeric(substring(Year, 2))) %>%
+    replace_na(list(N = 0))
+
+
+# Get ribbon values
+
+plotDF <- plotDF %>%
+    mutate(orderedName = factor(Name, levels = citationDF$Name)) %>%
+    group_by(Year) %>%
+    arrange(orderedName) %>%
+    mutate(
+        y2 = cumsum(N),
+        y1 = y2 - N
+    ) %>%
+    ungroup()
+
+# Plot
+
+cumulativePlot <- ggplot() + theme_bw(base_size = 18) +
+    geom_ribbon(data = plotDF, mapping = aes(x = Year, ymin = y1, ymax = y2, fill = orderedName)) +
+    scale_fill_manual(name = "Publication", values = scico(n = nrow(citationDF), palette = "batlow"), breaks = rev(levels(plotDF$orderedName))) +
+    scale_y_continuous(name = "# Citations", expand = c(0, 0), limits = c(0, 1.05 * max(plotDF$y2))) +
+    scale_x_continuous(breaks = 1995:2018, expand = c(0, 0)) +
+    theme(
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        panel.grid.minor.x = element_blank()
+    )
+
+png("../citations_cumulative.png", width = 800, height = 450)
+plot(cumulativePlot)
+dummy <- dev.off()
+
+svg("../citations_cumulative.svg", width = 800/72, height = 450/72)
+plot(cumulativePlot)
+dummy <- dev.off()
+
